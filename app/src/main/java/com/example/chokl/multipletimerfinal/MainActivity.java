6508ba1a8e2653d06755f3ca224ad54a4b0013da
relberger <relberger96@gmail.com>
2019-01-08 00:24:14 -0500

    timer input

--- app/src/main/java/com/example/chokl/multipletimerfinal/MainActivity.java
+++ app/src/main/java/com/example/chokl/multipletimerfinal/MainActivity.java
@@ -10,11 +10,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.InputFilter;
 import android.text.InputType;
+import android.text.Spanned;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -23,7 +26,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity
 {
     private TimerRowsAdapter timerRowsAdapter;
-    private ArrayList<Countdown> countdowns = new ArrayList<>() ;
+    private ArrayList<Countdown> countdowns = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -90,9 +93,9 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-    private void toggleMenuItem (MenuItem item)
+    private void toggleMenuItem(MenuItem item)
     {
-        item.setChecked (!item.isChecked ());
+        item.setChecked(!item.isChecked());
     }
 
     public void setupTimers()
@@ -157,15 +160,15 @@ public class MainActivity extends AppCompatActivity
     {
         final Countdown countdown = new Countdown();
 
-        AlertDialog.Builder inputAlert = new AlertDialog.Builder(this);
-        inputAlert.setTitle("Enter the timer name");
+        AlertDialog.Builder inputLabelAlert = new AlertDialog.Builder(this);
+        inputLabelAlert.setTitle("Enter the timer name");
 
         final EditText labelInput = new EditText(this);
         labelInput.setInputType(InputType.TYPE_CLASS_TEXT);
 
-        inputAlert.setView(labelInput);
+        inputLabelAlert.setView(labelInput);
 
-        inputAlert.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        inputLabelAlert.setPositiveButton("OK", new DialogInterface.OnClickListener()
         {
             @Override
             public void onClick(DialogInterface dialog, int which)
@@ -176,10 +179,133 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        inputAlert.show();
+        inputLabelAlert.show();
     }
 
     public void inputTime(View view)
     {
+        final Countdown countdown = new Countdown();
+
+        AlertDialog.Builder inputTimeAlert = new AlertDialog.Builder(this);
+        inputTimeAlert.setTitle("Enter the time");
+
+        final EditText timeHoursInput = new EditText(this);
+        timeHoursInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        final EditText timeMinutesInput = new EditText(this);
+        timeMinutesInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        final EditText timeSecondsInput = new EditText(this);
+        timeSecondsInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        final TextView colon1 = new TextView(this);
+        colon1.setText(":");
+        final TextView colon2 = new TextView(this);
+        colon2.setText(":");
+
+        final int maxLength = 2;
+        timeHoursInput.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+        timeMinutesInput.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+        timeSecondsInput.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+        timeHoursInput.setFilters(new InputFilter[]{new InputFilterMinMax("0", "23")});
+        timeMinutesInput.setFilters(new InputFilter[]{new InputFilterMinMax("0", "59")});
+        timeSecondsInput.setFilters(new InputFilter[]{new InputFilterMinMax("0", "59")});
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.addView(timeHoursInput);
+        layout.addView(colon1);
+        layout.addView(timeMinutesInput);
+        layout.addView(colon2);
+        layout.addView(timeSecondsInput);
+
+        inputTimeAlert.setView(layout);
+
+        inputTimeAlert.setPositiveButton("OK", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                String hours = "00";
+                if (!timeHoursInput.getText().toString().isEmpty())
+                {
+                    if (timeHoursInput.getText().toString().length() == 1)
+                    {
+                        hours = "0" + timeHoursInput.getText().toString();
+                    }
+                    else
+                    {
+                        hours = timeHoursInput.getText().toString();
+                    }
+                }
+
+                String minutes = "00";
+                if (!timeMinutesInput.getText().toString().isEmpty())
+                {
+                    if (timeMinutesInput.getText().toString().length() == 1)
+                    {
+                        minutes = "0" + timeMinutesInput.getText().toString();
+                    }
+                    else
+                    {
+                        minutes = timeMinutesInput.getText().toString();
+                    }
+                }
+                String seconds = "00";
+                if (!timeSecondsInput.getText().toString().isEmpty())
+                {
+                    if (timeSecondsInput.getText().toString().length() == 1)
+                    {
+                        seconds = "0" + timeSecondsInput.getText().toString();
+                    }
+                    else
+                    {
+                        seconds = timeSecondsInput.getText().toString();
+                    }
+                }
+
+                countdown.setRemainingTimeString(hours + minutes + seconds);
+                TextView time = findViewById(R.id.time);
+                time.setText(hours + ":" + minutes + ":" + seconds);
+            }
+        });
+
+        inputTimeAlert.show();
+    }
+
+    public class InputFilterMinMax implements InputFilter
+    {
+        //input filter taken from https://acomputerengineer.wordpress.com/2015/12/16/limit-number-range-in-edittext-in-android-using-inputfilter/
+
+        private int min, max;
+
+        public InputFilterMinMax(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public InputFilterMinMax(String min, String max)
+        {
+            this.min = Integer.parseInt(min);
+            this.max = Integer.parseInt(max);
+        }
+
+        private boolean isInRange(int a, int b, int c)
+        {
+            return b > a ? c >= a && c <= b : c >= b && c <= a;
+        }
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend)
+        {
+            try
+            {
+                int input = Integer.parseInt(dest.toString() + source.toString());
+                if (isInRange(min, max, input))
+                    return null;
+            }
+            catch (NumberFormatException nfe)
+            {
+            }
+            return "";
+        }
     }
 }
